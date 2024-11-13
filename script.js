1d2c827550677c6ce60597806fc518678d7cba65
aus1989 <151672980+aus1989@users.noreply.github.com>
2024-11-13 17:26:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideBar = document.getElementById("sideBar");
+
+function showSidebar(){
+  sideBar.style.display = "flex";
+}
+
+function closeSidebar(){
+  sideBar.style.display = "none";
+}
